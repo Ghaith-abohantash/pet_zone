3661cbc92725266998f16581e57be2297bbd1f54
bayan7abdalhq <s12114374@stu.najah.edu>
2025-05-07 23:10:21 +0300

    ch1

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         textTheme: GoogleFonts.poppinsTextTheme(),
         //to do
       ),
-      home: BuyPets(),
+      home: HomePage(),
     );
   }
 }
