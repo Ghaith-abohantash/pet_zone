202fc8fb025e6c5b1b4153a8d067ca1e2b0cf866
Ghaith Abohantash <s12011251@stu.najah.edu>
2025-05-16 20:39:31 +0300

    Adding VerificationScreen & ResetPasswordScreen & ForgetPasswordScreen

--- frontend/lib/routes/routes.dart
+++ frontend/lib/routes/routes.dart
@@ -4,6 +4,14 @@ import '../screens/home_doctor_screen.dart';
 import '../screens/welcome_screen.dart';
 import '../screens/appointments_screen.dart';
 import '../screens/veterinarian_notification_screen.dart';
+import '../screens/payment_screen.dart';
+import '../screens/forget _password_screen.dart';
+import '../screens/verification_screen.dart';
+import '../screens/reset_password_screen.dart';
+
+
+
+
 
 class AppRoutes {
   static const String splash = '/';
@@ -11,6 +19,13 @@ class AppRoutes {
   static const String welcomePage = '/welcomepage';
   static const String appointmentScreen = '/appointmentScreen';
   static const String veterinariannotificationscreen = '/veterinariannotificationscreen';
+  static const String paymentscreen = '/paymentscreen';
+  static const String forgetpasswordscreen = '/forgetpasswordscreen';
+  static const String verificationscreen = '/verificationscreen';
+  static const String resetpasswordscreen = '/resetpasswordscreen';
+
+
+
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
@@ -24,6 +39,14 @@ class AppRoutes {
         return MaterialPageRoute(builder: (_) => const AppointmentScreen());
       case veterinariannotificationscreen:
         return MaterialPageRoute(builder: (_) => const VeterinarianNotificationScreen());
+      case paymentscreen:
+        return MaterialPageRoute(builder: (_) => const PaymentScreen());
+      case forgetpasswordscreen:
+        return MaterialPageRoute(builder: (_) => const ForgetPasswordScreen());
+      case verificationscreen:
+        return MaterialPageRoute(builder: (_) => const VerificationScreen());
+      case resetpasswordscreen:
+        return MaterialPageRoute(builder: (_) => const ResetPasswordScreen());
       default:
         return MaterialPageRoute(builder: (_) => const SplashScreen());
     }
