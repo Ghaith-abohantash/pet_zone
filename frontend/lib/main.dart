0a3db4e0cfa0b6647e696db687b4daedbde9addf
bayan7abdalhq <s12114374@stu.najah.edu>
2025-05-17 14:57:31 +0300

    test

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,15 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:petzone_project/screens/add_animal_screen.dart';
-import 'package:petzone_project/screens/adopt_pet_details_screen.dart';
-import 'package:petzone_project/screens/animal_shop_screen.dart';
-import 'package:petzone_project/screens/buy_pets_screen.dart';
-import 'package:petzone_project/screens/chat_screen.dart';
-import 'package:petzone_project/screens/doctor_account_screen.dart';
-import 'package:petzone_project/screens/message_screen.dart';
-import 'package:petzone_project/screens/pets_details_form.dart';
 import 'routes/routes.dart';
-import 'package:petzone_project/screens/sale_pet_deatails_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,19 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Pet Zone App',
-      theme: ThemeData(
-        textTheme: GoogleFonts.poppinsTextTheme(),
-        //to do
-      ),
-      home: AddAnimalScreen(),
+      title: 'Test',
+      debugShowCheckedModeBanner: false,
 
-      //   debugShowCheckedModeBanner: false,
-    //
-    //   initialRoute: AppRoutes.splash,
-    //
-    //   onGenerateRoute: AppRoutes.generateRoute,
-    //
-        );
+      initialRoute: AppRoutes.splash,
+
+      onGenerateRoute: AppRoutes.generateRoute,
+    );
   }
 }
